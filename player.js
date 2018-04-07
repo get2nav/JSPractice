0986687ce368aaf2213cd467843fabc36c718ae8
get2nav <get2nav@gmail.com>
2018-04-07 15:20:19 -0400

    Final commit

--- player.js
+++ player.js
@@ -1,16 +1,19 @@
+/* DOM for getting the texts */
 var leftInput = document.getElementById("left-input");
 var rightInput = document.getElementById("right-input");
 
+/* Function variable for setInterval(Global)*/
 var leftLoop;
 var rightLoop;
 
+/* Pauses the text transfer */
 function pause() {
-    clearInterval(leftLoop);
+    clearInterval(leftLoop); 
     clearInterval(rightLoop);
 };
 
-
-function towards_right() {
+/* Moves the text from left textbox to right textbox */
+function leftToRight() {
     clearInterval(leftLoop);
     clearInterval(rightLoop);
 
@@ -18,24 +21,21 @@ function towards_right() {
         if (leftInput.value.length !== 0) {
             rightInput.value += leftInput.value.slice(0, 1);
             leftInput.value = leftInput.value.slice(1, leftInput.value.length);
-            rightInput.value = rightInput.value;
-            leftInput.value = leftInput.value;
         } else {
             clearInterval(leftLoop);
         }
     }, 1000);
 };
 
-function towards_left(){
+/* moves the text from right textbox to left textbox */
+function rightToLeft(){
     clearInterval(leftLoop);
     clearInterval(rightLoop);
     
     rightLoop = setInterval(function () {
         if (rightInput.value.length !== 0) {
-            leftInput.value = rightInput.value.slice(rightInput.value.length - 1, rightInput.value.length) + leftInput.value;
+            leftInput.value += rightInput.value.slice(rightInput.value.length - 1, rightInput.value.length);
             rightInput.value = rightInput.value.slice(0, rightInput.value.length - 1);
-            rightInput.value = rightInput.value;
-            leftInput.value = leftInput.value;
         } else {
             clearInterval(rightLoop);
         }
