b11dde39ee5c8d957bec8f91a32689eaff0dc622
get2nav <get2nav@gmail.com>
2018-04-07 20:15:43 -0400

    Revert "Final commit"
    This reverts commit 0986687ce368aaf2213cd467843fabc36c718ae8.

--- player.js
+++ player.js
@@ -1,19 +1,16 @@
-/* DOM for getting the texts */
 var leftInput = document.getElementById("left-input");
 var rightInput = document.getElementById("right-input");
 
-/* Function variable for setInterval(Global)*/
 var leftLoop;
 var rightLoop;
 
-/* Pauses the text transfer */
 function pause() {
-    clearInterval(leftLoop); 
+    clearInterval(leftLoop);
     clearInterval(rightLoop);
 };
 
-/* Moves the text from left textbox to right textbox */
-function leftToRight() {
+
+function towards_right() {
     clearInterval(leftLoop);
     clearInterval(rightLoop);
 
@@ -21,23 +18,44 @@ function leftToRight() {
         if (leftInput.value.length !== 0) {
             rightInput.value += leftInput.value.slice(0, 1);
             leftInput.value = leftInput.value.slice(1, leftInput.value.length);
+            rightInput.value = rightInput.value;
+            leftInput.value = leftInput.value;
         } else {
             clearInterval(leftLoop);
         }
     }, 1000);
 };
 
+<<<<<<< HEAD
 /* moves the text from right textbox to left textbox */
 function rightToLeft(){ 
+||||||| 0986687... Final commit
+/* moves the text from right textbox to left textbox */
+function rightToLeft(){
+    clearInterval(leftLoop);
+=======
+function towards_left(){
+    clearInterval(leftLoop);
+>>>>>>> parent of 0986687... Final commit
     clearInterval(rightLoop);
     clearInterval(leftLoop);
     
     rightLoop = setInterval(function () {
         if (rightInput.value.length !== 0) {
+<<<<<<< HEAD
             // leftInput.value += rightInput.value.slice(rightInput.value.length - 1, rightInput.value.length);
             // rightInput.value = rightInput.value.slice(0, rightInput.value.length - 1);
             leftInput.value += rightInput.value.slice(0, 1);
             rightInput.value = rightInput.value.slice(1, rightInput.value.length);
+||||||| 0986687... Final commit
+            leftInput.value += rightInput.value.slice(rightInput.value.length - 1, rightInput.value.length);
+            rightInput.value = rightInput.value.slice(0, rightInput.value.length - 1);
+=======
+            leftInput.value = rightInput.value.slice(rightInput.value.length - 1, rightInput.value.length) + leftInput.value;
+            rightInput.value = rightInput.value.slice(0, rightInput.value.length - 1);
+            rightInput.value = rightInput.value;
+            leftInput.value = leftInput.value;
+>>>>>>> parent of 0986687... Final commit
         } else {
             clearInterval(rightLoop);
         }
